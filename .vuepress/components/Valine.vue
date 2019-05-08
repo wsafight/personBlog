97569115f28d8a6eca3339b4fa1f5894caf40f15
wsa <984292420>
2019-05-08 22:16:33 +0800

    valine 修整

--- .vuepress/components/Valine.vue
+++ .vuepress/components/Valine.vue
@@ -1,10 +1,44 @@
 <template>
-  <div id="vcomments"></div>
+  <div >
+    <section>  
+      <div id="vcomments"></div>
+       <!-- id 将作为查询条件 -->
+        <span class="leancloud-visitors"
+              data-flag-title="Your Article Title">
+          <em class="post-meta-item-text">阅读量： </em>
+          <i class="leancloud-visitors-count"></i>
+        </span>
+    </section>
+  </div>
 </template>
-
 <script>
 export default {
   name: 'Valine',
+   watch: {
+    $route (to, from) {
+      if (from.path != to.path) {
+        this.initValine()
+      }
+    }
+  },
+  methods: {
+    initValine () {
+      let path = location.origin + location.pathname
+      // vuepress打包后变成的HTML不知为什么吞掉此处的绑定`:id="countId"`
+      document.getElementsByClassName('leancloud-visitors')[0].id = path
+      this.valine.init({
+        el: '#vcomments',
+        appId: 'JsbIEIrpTtaEWhGXAC4vGHWm-gzGzoHsz',// your appId
+        appKey: '3M0AXkI6H2vjBtjELX0gPfHR', // your appKey
+        notify: false, 
+        verify: true, 
+        avatar:'wavatar', 
+        placeholder: '请发表你的看法(支持markdown)',
+        path: path,
+        visitor: true
+      });
+    }
+  },
   mounted: function(){
     // require window 
     const Valine = require('valine');
@@ -14,15 +48,8 @@ export default {
       
     }
      
-    new Valine({
-      el: '#vcomments' ,
-      appId: 'JsbIEIrpTtaEWhGXAC4vGHWm-gzGzoHsz',// your appId
-      appKey: '3M0AXkI6H2vjBtjELX0gPfHR', // your appKey
-      notify: false, 
-      verify: true, 
-      avatar:'mm', 
-      placeholder: '请发表你的看法(支持markdown)' 
-    });
+    this.valine = new Valine()
+    this.initValine()
   },
 }
 </script>
