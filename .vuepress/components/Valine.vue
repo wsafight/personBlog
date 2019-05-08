3a1442a14741e47bbb1fe64f19d5509867623026
wsa <984292420>
2019-05-08 21:54:46 +0800

    添加评论

---
+++ .vuepress/components/Valine.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="vcomments"></div>
+</template>
+
+<script>
+export default {
+  name: 'Valine',
+  mounted: function(){
+    // require window 
+    const Valine = require('valine');
+    if (typeof window !== 'undefined') {
+      this.window = window
+      window.AV = require('leancloud-storage')
+      
+    }
+     
+    new Valine({
+      el: '#vcomments' ,
+      appId: 'JsbIEIrpTtaEWhGXAC4vGHWm-gzGzoHsz',// your appId
+      appKey: '3M0AXkI6H2vjBtjELX0gPfHR', // your appKey
+      notify: false, 
+      verify: true, 
+      avatar:'mm', 
+      placeholder: '请发表你的看法(支持markdown)' 
+    });
+  },
+}
+</script>
