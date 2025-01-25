52fcba17ce78495d9dc80b3437afed7e7792f96c
jump-and-jump <984292420@qq.com>
2025-01-25 13:21:01 +0800

    feat: change GithubCardComponent

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import swup from "@swup/astro";
-import partytown from '@astrojs/partytown';
 import Compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
@@ -26,7 +25,6 @@ export default defineConfig({
   base: "personBlog",
   trailingSlash: "always",
   integrations: [
-    partytown(),
     tailwind({
       nesting: true,
     }),
